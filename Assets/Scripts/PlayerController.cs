186c1131f98e8ad1dd8996a2bf2fa5e77a0e66d8
kamikrv <kamilla.karayeva@mail.ru>
2022-11-30 14:07:45 +0600

    player can sit in to the car

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] GameObject Car;
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Vector3.Distance(Player.transform.position, Car.transform.position));
+        if((Vector3.Distance(Player.transform.position, Car.transform.position) < 3) && (Input.GetKeyDown(KeyCode.E)))
+        {
+            Player.transform.SetParent(Car.transform);
+            Player.SetActive(false);
+            Car.SetActive(true);
+            Camera.GetComponent<CinemachineVirtualCamera>().LookAt = Car.transform;
+            Camera.GetComponent<CinemachineVirtualCamera>().Follow = Car.transform;
+            Car.GetComponent<PrometeoCarController>().enabled = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.R)){
+            Player.transform.SetParent(null);
+            Player.SetActive(true);
+            Camera.GetComponent<CinemachineVirtualCamera>().LookAt = Player.transform;
+            Camera.GetComponent<CinemachineVirtualCamera>().Follow = Player.transform;
+            Car.GetComponent<PrometeoCarController>().enabled = false;
+        }
+    }
+}
